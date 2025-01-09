ac517ecb1bf3df15efc5df5f20de08bf95855ddb
菜菜驴 <5407627+caicailv1@user.noreply.gitee.com>
2025-01-09 15:10:21 +0800

    test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello getHello!';
+    return 'Hello getHello!test';
   }
   getHee(): string {
     return 'Hello hee22222222222222222!';
