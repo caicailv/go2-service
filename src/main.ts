c0cd71e7c2a4e2f36ec6d9a12028a64832fb9e80
菜菜驴 <5407627+caicailv1@user.noreply.gitee.com>
2025-01-20 10:49:10 +0800

    ea

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { CustomLoggerService } from './logger.service';  // 引入自定义 Logger
+// import { CustomLoggerService } from './logger.service';  // 引入自定义 Logger
 
 import * as dotenv from 'dotenv';
 dotenv.config(); 
@@ -9,13 +9,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
-  const logger = app.get(CustomLoggerService);  // 获取 CustomLoggerService
-  app.useLogger(logger);  // 使用自定义的 Logger
+  // const logger = app.get(CustomLoggerService);  // 获取 CustomLoggerService
+  // app.useLogger(logger);  // 使用自定义的 Logger
   await app.listen(3322);
 }
 console.log('change v1');
-console.log(`process.env.MYSQL_HOST`,process.env.MYSQL_HOST);
-console.log(`1`,process.env.MYSQL_HOST);
-console.log(`UPYUN_SERVICE_NAME`,process.env.UPYUN_SERVICE_NAME);
 console.log(`2`,process.env.UPYUN_SERVICE_NAME);
 bootstrap();
